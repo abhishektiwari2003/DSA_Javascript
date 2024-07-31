826e99fc3d62605ca6a6e65dc464778de28c3f39
Abhishek Tiwari <tiwari.abhishek2021@vitbhopal.ac.in>
2024-07-31 13:22:41 +0530

    Initial Commit

---
+++ dsa.js
@@ -0,0 +1,29 @@
+// O(n) understanding
+
+// Here the number of operations is going to be performed for which n is defined too.
+
+// function linearLoop(n) {
+//   {
+//     for (let i = 0; i < n; i++) {
+//       console.log(i);
+//     }
+//   }
+// }
+
+// linearLoop(11);
+
+// O(n^2) understanding
+
+// Here the number of operations performed will be square of n is defined
+
+// function quadraticLoop(n) {
+//   {
+//     for (let i = 0; i < n; i++) {
+//       for (let j = 0; j < n; j++) {
+//         console.log(i, j);
+//       }
+//     }
+//   }
+// }
+
+// quadraticLoop(10);
